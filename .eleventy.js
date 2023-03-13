c29d3a65ee5ed9011aec5546ea9a2b4b9d8f39ae
Jan Anhalt <77264975+anhaltjan@users.noreply.github.com>
2023-03-13 18:37:16 +0900

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+  /* Always allow in the public folder */
+  eleventyConfig.addPassthroughCopy('./src/css/')
+  eleventyConfig.addPassthroughCopy('./src/assets/')
+  eleventyConfig.addPassthroughCopy('./src/pages/')
+  /* Watch for updates */
+  eleventyConfig.addWatchTarget('./src/css/')
+  eleventyConfig.addWatchTarget('./src/pages')
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: 'public',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  }
+}
